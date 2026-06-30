d5d26fb7eeda20953064622d88b62a1de689c26a
Andre Krautschick <andre.krautschick@eurocontrol.int>
2026-06-30 13:25:45 +0000

    Execption is thrown during connection setup when no jar files are available.

--- tasks.py
+++ tasks.py
@@ -43,7 +43,29 @@ def atests(context):
     global atests_completed_process
     atests_completed_process = subprocess.run(" ".join(cmd), shell=True, check=False)
 
-@task(atests)
+@task
+def atests_failure_case(context):
+    cmd = [
+        "coverage",
+        "run",
+        "--source=JMS",
+        "-p",
+        "-m",
+        "robot",
+        "--loglevel=TRACE:DEBUG",
+        "--listener RobotStackTracer",
+        "--exclude appiumORnot_readyORnot_ci",
+        "-d results",
+        "-o output_failure_case.xml",
+        "-l log_failure_case.html",
+        "-r report_failure_case.html",
+        "--prerebotmodifier utilities.xom.XUnitOut:results/xunit_failure_case.xml",
+        f"{ROOT}/tests/atest_failure_case"
+    ]
+    global atests_completed_process
+    atests_completed_process = subprocess.run(" ".join(cmd), shell=True, check=False)
+
+@task(atests,atests_failure_case)
 def tests(context):
     subprocess.run("coverage combine", shell=True, check=False)
     subprocess.run("coverage report", shell=True, check=False)
